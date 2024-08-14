524670f75f62c40a767414f43c050dd7a10bc0b6
Onirim <onirim@outlook.com>
2024-08-14 11:56:32 +0200

    Updating for Wow 11.0.2
    Fixing error with C_AddOns.GetAddOnMetadata GetAddOnMetadata is deprecated)

--- Markers.lua
+++ Markers.lua
@@ -1,7 +1,7 @@
 -- Gestion de la localisation
 local _, core = ...
 local L = core.Locales[GetLocale()] or core.Locales["enUS"]
-local version = GetAddOnMetadata("DMTools", "Version")
+local version = C_AddOns.GetAddOnMetadata("DMTools", "Version")
 
 local markerSync = false
 local colorWhite = "|cFFFFFFFF"
